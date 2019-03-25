bc816ea445cbe8778afa54da725c831ea78322c9
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-24 23:11:58 -0500

    udemy redux blog complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,15 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, "userId"));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // lodash _.chain function
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
